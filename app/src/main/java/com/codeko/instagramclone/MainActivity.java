a15398fee003a3476c14b5e32e9aa2165556d390
Cuong Ngo <cuong.ngo@ascendcorp.com>
2018-03-15 23:03:54 +0700

    Sign up

--- app/src/main/java/com/codeko/instagramclone/MainActivity.java
+++ app/src/main/java/com/codeko/instagramclone/MainActivity.java
@@ -2,6 +2,14 @@ package com.codeko.instagramclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void signUp(View view) {
+        EditText username = findViewById(R.id.edtUsername);
+        EditText password = findViewById(R.id.edtPassword);
+
+        if (username.getText().toString().matches("") || password.getText().toString().matches("")) {
+            Toast.makeText(this, "A username and password are required", Toast.LENGTH_LONG).show();
+        } else {
+            ParseUser user = new ParseUser();
+            user.setUsername(username.getText().toString());
+            user.setPassword(password.getText().toString());
+
+            user.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        Log.d("login", "successful");
+                    } else {
+                        Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
     }
 }
